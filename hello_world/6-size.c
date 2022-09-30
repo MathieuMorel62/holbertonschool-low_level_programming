5f77609a496ea92af286f15a2d319556de1010b2
MathieuMorel62 <mormth@aol.com>
2022-09-30 07:41:12 -0700

    delete s

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -14,11 +14,11 @@ long int longIntType;
 long long int longLongIntType;
 float floatType;
 
-printf("Size of a char: %zu bytes\n", sizeof(charType));
-printf("Size of a int: %zu bytes\n", sizeof(intType));
-printf("Size of a long int: %zu bytes\n", sizeof(longIntType));
-printf("Size of a long long int: %zu bytes\n", sizeof(longLongIntType));
-printf("Size of a float: %zu bytes\n", sizeof(floatType));
+printf("Size of a char: %zu byte\n", sizeof(charType));
+printf("Size of a int: %zu byte\n", sizeof(intType));
+printf("Size of a long int: %zu byte\n", sizeof(longIntType));
+printf("Size of a long long int: %zu byte\n", sizeof(longLongIntType));
+printf("Size of a float: %zu byte\n", sizeof(floatType));
 
 return (0);
 }
