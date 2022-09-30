120e2f5ee0711c74be0326ac43fbdd9e0b9c1b57
MathieuMorel62 <mormth@aol.com>
2022-09-30 03:19:13 -0700

    function puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
-int puts("Programming is like building a multilingual puzzle");
+/**
+ * main - Entry point
+ *
+ * Description: puts
+ *
+ * Return: Always 0 (Success)
+ */
 
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
+}
