c0e22496455ac21945e417f92738b3cd826d1b69
MathieuMorel62 <mormth@aol.com>
2022-10-03 08:20:52 -0700

    variable base 10

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: that prints the alphabet in lowercase
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int num;
+
+for (num = 0; num < 10; num++)
+{
+putchar(n);
+}
+putchar("\n);
+return (0);
+}
+
