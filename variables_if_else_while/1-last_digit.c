6854534061f36a0bde901da0d6b8558691940939
MathieuMorel62 <mormth@aol.com>
2022-10-03 05:09:09 -0700

    add variable

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This program will assign a random number to the variable n each
+ *              time it is executed. Complete the source code in order to print
+ *              the last digit of the number stored in the variable n.
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+printf("Last digit of %d and is less than %d, n, n % 10 \n ", n);
+if (n % 10 > 5)
+printf("and is greater thank 5\n", n);
+else if (n % 10 == 0)
+printf("and is 0\n", n);
+else if (n % 10 < 6 != 0)
+printf("and is less than 6 and not 0\n", n);
+return (0);
