c01da104c34c1a92a53491318c952699dee96667
MathieuMorel62 <mormth@aol.com>
2022-10-03 06:17:21 -0700

    try file

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: that prints the alphabet in lowercase
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char x;
+
+for( x = 'a'; x <= 'z'; x++ )
+putchar(x);
+return (0);
+}
