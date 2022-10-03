b438ba0dfbb7d5a4b4fe7fe4aa43e804d5ef4b7c
MathieuMorel62 <mormth@aol.com>
2022-10-03 05:26:42 -0700

    delete space

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 
-printf("Last digit of %d is %d ", n, n % 10);
+printf("Last digit of %d is %d", n, n % 10);
 if (n % 10 > 5)
 printf("and is greater thank 5\n");
 else if (n % 10 == 0)
