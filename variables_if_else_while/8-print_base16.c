76d8e7680c3bab6374fd3fc6b6d6a017bfd189fa
MathieuMorel62 <mormth@aol.com>
2022-10-03 09:02:35 -0700

    function base 16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: that prints the alphabet in lowercase
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int num;
+int letter;
+
+for (num = 0; num < 9; num++)
+{
+putchar(num);
+}
+for (letter = 'a'; letter <= 'f'; letter++)
+{
+putchar(letter);
+}
+putchar('\n');
+return (0);
+}
+
