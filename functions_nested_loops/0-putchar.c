29a1589202dd35e357497607718653194c222f3e
Holberton School <mormth@aol.com>
2022-10-04 10:51:01 +0200

    file putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -12,7 +12,15 @@
 
 int main(void)
 {
-_printf("_putchar");
+_putchar('_')
+_putchar('p')
+_putchar('u')
+_putchar('t')
+_putchar('c')
+_putchar('h')
+_putchar('a')
+_putchar('r')
 putchar('\n');
+
 return (0);
 }
