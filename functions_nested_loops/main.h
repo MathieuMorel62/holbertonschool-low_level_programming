19cd88eef24888c6c3aac29a87655adb1b7ae2d7
Holberton School <mormth@aol.com>
2022-10-04 11:09:39 +0200

    add file putchar

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,1 +1,13 @@
-int _printf(char c);
+#ifdef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ *
+ * Description: Header file containing declarations
+ *              for all functions used in the 0x02-
+ *              functions_nested_loops directory.
+ */
+
+
+int _putchar(char c);
