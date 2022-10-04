98fbf60ab86101889d4078e37343d5889cd9a996
Holberton School <mormth@aol.com>
2022-10-04 10:19:16 +0200

    add file print _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: program that prints _putchar, 
+ *              followed by a new line.
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+printf("_putchar");
+putchar('\n');
+return (0);
+}
