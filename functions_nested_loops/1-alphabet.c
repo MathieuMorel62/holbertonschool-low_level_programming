7772376910a8f3bb523541b3a3bf3466c160a5a7
Holberton School <mormth@aol.com>
2022-10-04 12:56:25 +0200

    file 1-alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: that prints the alphabet in lowercase
+ *
+ *Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+_putchar(alphabet);
+_putchar('\n');
+return (0);
+}
