83281dd3d18740927331844921e6edec2a8b7b3d
Holberton School <mormth@aol.com>
2022-10-04 11:45:42 +0200

    add file

--- functions_nested_loops/_putchar.c
+++ functions_nested_loops/_putchar.c
@@ -1,1 +1,16 @@
-ok
+#include <unistd.h>
+
+/**
+ * File: _putchar.c
+ *
+ * Description: _putchar writes the character 'c' to stdout
+ * 'c': the character to print
+ * 
+ * Return: On success 1.
+ * On error, -1 is returned, and error is set appropriately.
+ */
+
+int_putchar(char c)
+{
+return (write(1, &c, 1));
+}
