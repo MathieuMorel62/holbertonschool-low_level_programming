58bc2eb7e82627af76858f487663a88b8e243582
MathieuMorel62 <mormth@aol.com>
2022-10-07 14:35:09 -0700

    indentation

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -8,11 +8,12 @@
 
 void print_alphabet(void)
 {
-char alphabet;
+	char alphabet;
 
-for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-_putchar(alphabet);
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 
-_putchar('\n');
+		_putchar(alphabet);
+
+	_putchar('\n');
 }
 
