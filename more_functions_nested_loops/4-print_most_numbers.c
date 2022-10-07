db9806fbb31c84f342472a665f37914dbfb03161
MathieuMorel62 <mormth@aol.com>
2022-10-07 15:23:28 -0700

    indentation

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -9,13 +9,14 @@
 
 void print_most_numbers(void)
 {
-int x;
+	int x;
 
-for (x = '0'; x <= '9'; x++)
-{
-if (x != '2' && x != '4')
-_putchar(x);
-}
-_putchar('\n');
+	for (x = '0'; x <= '9'; x++)
+	{
+		if (x != '2' && x != '4')
+
+			_putchar(x);
+	}
+	_putchar('\n');
 }
 
