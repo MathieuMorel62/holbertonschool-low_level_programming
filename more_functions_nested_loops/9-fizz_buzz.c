35d70b71dceac39a40d806e5bd35392e07c48956
MathieuMorel62 <mormth@aol.com>
2022-10-07 01:08:57 -0700

    change > l21

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -16,19 +16,19 @@
 
 int main(void)
 {
-int a;
+int n;
 
-for (a = 1; a >= 100; a++)
+for (n = 1; n <= 100; n++)
 {
-if (a % 3 == 0 && a % 5 == 0)
+if (n % 3 == 0 && n % 5 == 0)
 printf("FizzBuzz");
-else if (a % 5 == 0)
+else if (n % 5 == 0)
 printf("Buzz");
-else if (a % 3 == 0)
+else if (n % 3 == 0)
 printf("Fizz");
 else
-printf("%d", a);
-if (a < 100)
+printf("%i", n);
+if (n < 100)
 printf(" ");
 }
 printf("\n");
