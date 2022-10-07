4ab1247ec8c0913f089a36c5c21c810150c60bcd
MathieuMorel62 <mormth@aol.com>
2022-10-07 15:02:40 -0700

    indentation

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -5,23 +5,24 @@
   *
   * Return: ...
   */
+
 void jack_bauer(void)
 {
 
-int hours;
-int minutes;
+	int hours;
+	int minutes;
 
-for (hours = 0; hours < 24; hours++)
-{
-for (minutes = 0; minutes < 60; minutes++)
-{
-_putchar((hours / 10) + '0');
-_putchar((hours % 10) + '0');
-_putchar(':');
-_putchar((minutes / 10) + '0');
-_putchar((minutes % 10) + '0');
-_putchar('\n');
-}
-}
+	for (hours = 0; hours < 24; hours++)
+	{
+		for (minutes = 0; minutes < 60; minutes++)
+		{
+			_putchar((hours / 10) + '0');
+			_putchar((hours % 10) + '0');
+			_putchar(':');
+			_putchar((minutes / 10) + '0');
+			_putchar((minutes % 10) + '0');
+			_putchar('\n');
+		}
+	}
 }
 
