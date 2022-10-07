ddcf1a526021201811adad5ae00aa7b864987320
MathieuMorel62 <mormth@aol.com>
2022-10-07 14:14:16 -0700

    indentation

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -13,11 +13,14 @@
 
 int main(void)
 {
-int num;
+	int num;
 
-for (num = 0; num < 10; num++)
-printf("%d", num);
-putchar('\n');
-return (0);
+	for (num = 0; num < 10; num++)
+
+		printf("%d", num);
+
+	putchar('\n');
+
+	return (0);
 }
 
