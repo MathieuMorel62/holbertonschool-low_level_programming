3062ab76ddd859e0d3e77b3f63eccb5ba0cddc79
MathieuMorel62 <mormth@aol.com>
2022-10-07 15:34:59 -0700

    indentation

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -16,22 +16,28 @@
 
 int main(void)
 {
-int n;
+	int n;
 
-for (n = 1; n <= 100; n++)
-{
-if (n % 3 == 0 && n % 5 == 0)
-printf("FizzBuzz");
-else if (n % 5 == 0)
-printf("Buzz");
-else if (n % 3 == 0)
-printf("Fizz");
-else
-printf("%i", n);
-if (n < 100)
-printf(" ");
-}
-printf("\n");
-return (0);
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % 3 == 0 && n % 5 == 0)
+			printf("FizzBuzz");
+
+		else if (n % 5 == 0)
+			printf("Buzz");
+
+		else if (n % 3 == 0)
+			printf("Fizz");
+
+		else
+			printf("%i", n);
+
+		if (n < 100)
+			printf(" ");
+
+	}
+	printf("\n");
+
+	return (0);
 }
 
