57089e029a058d83197bddd0000e1935d2196dfc
MathieuMorel62 <mormth@aol.com>
2022-10-07 15:37:17 -0700

    indentation

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -9,19 +9,21 @@
 void print_triangle(int size)
 {
 
-int a;
-int b;
+	int a;
+	int b;
 
-if (size < 1)
-_putchar('\n');
+	if (size < 1)
+		_putchar('\n');
 
-for (a = 1; a <= size; a++)
-{
-for (b = size - a; b > 0; b--)
-_putchar(' ');
-for (b = a; b > 0; b--)
-_putchar('#');
-_putchar('\n');
-}
+	for (a = 1; a <= size; a++)
+	{
+		for (b = size - a; b > 0; b--)
+			_putchar(' ');
+
+		for (b = a; b > 0; b--)
+			_putchar('#');
+
+		_putchar('\n');
+	}
 }
 
