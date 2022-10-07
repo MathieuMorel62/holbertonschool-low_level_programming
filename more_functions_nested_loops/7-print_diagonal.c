d117d00d5690bea101843c5f77e23efaa79aa9bc
MathieuMorel62 <mormth@aol.com>
2022-10-07 15:29:58 -0700

    indentation

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -10,19 +10,20 @@
 
 void print_diagonal(int n)
 {
-int draw, space;
+	int draw, space;
 
-if (n > 0)
-{
-for (draw = 1; draw <= n; draw++)
-{
-for (space = 1; space < draw; space++)
-_putchar(' ');
-_putchar('\\');
-_putchar('\n');
-}
-}
-else
-_putchar('\n');
+	if (n > 0)
+	{
+		for (draw = 1; draw <= n; draw++)
+		{
+			for (space = 1; space < draw; space++)
+
+				_putchar(' ');
+				_putchar('\\');
+				_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
 }
 
