ae6e1f36fdc3d65632fc4b85ce0cbe77e7166de8
MathieuMorel62 <mormth@aol.com>
2022-10-10 12:54:47 -0700

    add for

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -11,16 +11,11 @@ void print_rev(char *s)
 {
 	int i = 0;
 
-	while (*(s + i))
-	{
+	while (s[i] != '\0')
 		i++;
-	}
-	i = i - 1;
 
-	while (i >= 0)
-	{
-		_putchar (*(s + i));
-		i--;
-	}
-	_putchar ('\n');
+	for (i = i - 1; i >= 0; i--)
+		_putchar(s[i]);
+
+	_putchar('\n');
 }
