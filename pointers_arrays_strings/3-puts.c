d30b2c144f7692b5ee772a23eb4a469de9067bbf
MathieuMorel62 <mormth@aol.com>
2022-10-10 05:53:02 -0700

    task 3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - Prints string then newline to stdout
+ * @str: String being printed
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (*(str + i))
+	{
+		_putchar (*(str + i));
+			i++;
+	}
+	_putchar ('\n');
+}
+	
