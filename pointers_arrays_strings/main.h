3bd72e078c515ee18f22ef87b74cbeb6f8f174f7
MathieuMorel62 <mormth@aol.com>
2022-10-11 10:52:54 -0700

    add main file

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,4 +1,6 @@
-// Part 01
+/*
+ *  Part 01
+ */
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -12,7 +14,9 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
-// Part 02
+/*
+ * Part 02
+ */
 
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
