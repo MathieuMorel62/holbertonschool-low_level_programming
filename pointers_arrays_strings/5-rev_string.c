5cce8201fb07acc61f0662c016e229bacf24866b
MathieuMorel62 <mormth@aol.com>
2022-10-11 02:08:34 -0700

    task 5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string to reverse
+ *
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int i = 0;
+	int j = 0;
+
+	char temp[100];
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	for (; i > 0; i--)
+	{
+		temp[j] = s[i -1];
+		j++;
+	}
+
+	while (i < j)
+	{
+		s[i] = temp[i];
+		i++;
+	}
+}
