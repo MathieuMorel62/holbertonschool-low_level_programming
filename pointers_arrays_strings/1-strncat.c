f2c5eadda00f5d27d0cc494ac85cdb71c27144c9
MathieuMorel62 <mormth@aol.com>
2022-10-11 14:14:34 -0700

    change boucle while

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,25 +1,30 @@
 #include "main.h"
 #include <stdio.h>
 
-/*
- *  _strncat - Concatenates two strings
- *  @dest: String being concatenated
- *  @src: String being concatenated
- *  @n: Number of bytes
- *
- *  Return: pointer to resulting string dest
- */
-
+/**
+ **_strncat -main entry.
+ *void: nothing
+ *@dest: char pointer
+ *@src: char pointer
+ *@n: integer variable
+ *Description: function that concatenates two strings.
+ * Return:  pointer to the resulting string dest
+ **/
 char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0;
-	int j;
+	int j = 0;
 
 	while (dest[i] != '\0')
 		i++;
 
-	for (j = 0; j < n; j++, i++)
-		dest[i] = src[j];
+	while (src[j] != src[n])
+	{
+		dest[i] =src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
 
 	return (dest);
 }
