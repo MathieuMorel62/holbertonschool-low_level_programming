82978df83d892e1c8e17c8cdbb1d1749aebdbd8c
MathieuMorel62 <mormth@aol.com>
2022-10-12 01:52:08 -0700

    task 3compare 2 strings

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcmp - Compares two strings
+ * @s1: First string being evaluated
+ * @s2: Second string being evaluated
+ *
+ * Return: Difference between string characters being evaluated
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+
+	return (s1[i] - s2[i]);
+}
