b017126ceee8ed3604961a3c514a81328f96caa8
MathieuMorel62 <mormth@aol.com>
2022-10-12 03:39:00 -0700

    add betty style

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -2,7 +2,7 @@
 
 /**
  * cap_string - Capitalises all words of a string
- * @s: String being evaluated
+ * @chaine: String being evaluated
  *
  * Return: Capitalised string
  */
@@ -17,17 +17,17 @@ char *cap_string(char *chaine)
 		{
 			if (i == 0)
 				chaine[i] = chaine[i] - 32;
-			else if (chaine[i - 1] >= 9 && chaine[i -1] <= 10)
+			else if (chaine[i - 1] >= 9 && chaine[i - 1] <= 10)
 				chaine[i] = chaine[i] - 32;
-			else if (chaine[i - 1] >= 32 && chaine[i -1] <= 34)
+			else if (chaine[i - 1] >= 32 && chaine[i - 1] <= 34)
 				chaine[i] = chaine[i] - 32;
-			else if (chaine[i -1] >= 40 && chaine[i -1] <= 41)
+			else if (chaine[i - 1] >= 40 && chaine[i - 1] <= 41)
 				chaine[i] = chaine[i] - 32;
-			else if (chaine[i -1] == 46)
+			else if (chaine[i - 1] == 46)
 				chaine[i] = chaine[i] - 32;
 			else if (chaine[i - 1] == 59)
 				chaine[i] = chaine[i] - 32;
-			else if (chaine[i - 1] == 123 || chaine[i -1] == 125)
+			else if (chaine[i - 1] == 123 || chaine[i - 1] == 125)
 				chaine[i] = chaine[i] - 32;
 		}
 	}
