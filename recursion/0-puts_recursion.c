a86a967801a88bb94f776cc7c083eb978a8eee1a
MathieuMorel62 <mormth@aol.com>
2022-10-17 02:51:38 -0700

    task 0 recursion

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string, followed by a new line
+ * @s: Pointer to string being evaluated
+ */
+
+void _puts_recursion(char *s)
+{
+	
+	if (*s)
+	{
+		_putchar(*s);
+	_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+
+}
