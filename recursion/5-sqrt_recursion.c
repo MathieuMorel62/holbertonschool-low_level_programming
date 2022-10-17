3309532128e9a8786091175b0fb2b57163b5a420
MathieuMorel62 <mormth@aol.com>
2022-10-17 07:23:58 -0700

    task 5 returns the natural square root of a number

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * root - search the square number
+ * @n: Number one
+ * @i: number two
+ *
+ * Return: Square of number
+ */
+
+int root(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+
+	else if (i * i == n)
+		return (i);
+
+	else
+		return (root(n, i + 1));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square of a root
+ * @n: parameter
+ *
+ * return: natural square root of number
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (root(n, 1));
+}
