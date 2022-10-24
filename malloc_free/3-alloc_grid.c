b0dd3be33714c54a072bf46e1a3199911c63ef2d
MathieuMorel62 <mormth@aol.com>
2022-10-24 11:14:06 -0700

    add init array

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -12,7 +12,7 @@
 int **alloc_grid(int width, int height)
 {
 	int **res;
-	int index;
+	int index, init;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
@@ -32,7 +32,11 @@ int **alloc_grid(int width, int height)
 		{
 			return (NULL);
 		}
-	}
 
+		for (init = 0; init < width; init++)
+		{
+			res[index][init] = 0;
+		}
+	}
 	return (res);
 }
