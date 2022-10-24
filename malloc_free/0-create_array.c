738ab643356fe134995e763296c5aa9992a533ae
MathieuMorel62 <mormth@aol.com>
2022-10-24 01:56:55 -0700

    new directory

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - Creates array of chars then initialises
+ * @size: Size of the memory
+ * @c: Char to initialise with
+ *
+ * Return: Pointer to array, NULL if fails
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int index;
+
+	if (size == 0)
+		return (NULL);
+
+	array = malloc(sizeof(char) * size);
+
+	if (array == 0)
+		return (NULL);
+
+	for (index = 0; index < size; index++)
+		array[index] = c;
+
+	return (array);
+}
