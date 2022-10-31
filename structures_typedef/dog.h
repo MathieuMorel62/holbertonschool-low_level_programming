f1f2fb78624d14cf8d9c241bae415b8e95b20af6
MathieuMorel62 <mormth@aol.com>
2022-10-31 02:52:18 -0700

    add prototype

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,8 +1,25 @@
-#include <stdio.h>
+#ifndef DOG_H_
+#define DOG_H_
+
+/**
+ * struct dog - structure
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
 
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
