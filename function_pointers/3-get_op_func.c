7e2367966cc3af7476086ee931976b3f0cb67820
MathieuMorel62 <mormth@aol.com>
2022-11-02 03:21:34 -0700

    add files function

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * get_op_func - selects the correct function to perform operation
+ * @s: operation given (+, *, -, /, %)
+ * Return: pointer to correct operation
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+/**
+ * op_add - addition
+ * @a: integer number 1
+ * @b: integer number 2
+ * Return: result of operation
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtraction
+ * @a: integer number 1
+ * @b: integer number 2
+ * Return: result of operation
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplication
+ * @a: integer number 1
+ * @b: integer number 2
+ * Return: result of multiplication
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division
+ * @a: integer number 1
+ * @b: integer number 2
+ * Return: result of division
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - remainder of the division
+ * @a: integer number 1
+ * @b: integer number 2
+ * Return: result of modulo
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
