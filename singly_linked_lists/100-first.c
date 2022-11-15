8108452cbbc4605d63558c82003dadfb5a953830
MathieuMorel62 <mormth@aol.com>
2022-11-15 04:02:32 -0800

    function that prints text before the main is executed

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * myStartupFun - function that prints before the main is executed
+ */
+
+void myStartupFun (void)
+{
+	printf ("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
