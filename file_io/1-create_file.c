a9b8634a723387ba89aba88f1cde6c2b1ed29c50
MathieuMorel62 <mormth@aol.com>
2022-11-21 06:26:30 -0800

    change permission

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -10,24 +10,24 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fp;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (fd == -1)
+	fp = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fp == -1)
 		return (-1);
 
 	if (text_content == NULL)
 	{
-		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+		fp = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 		return (1);
 	}
 	else
-		fd = write(fd, text_content, strlen(text_content));
+		fp = write(fp, text_content, strlen(text_content));
 
-	close(fd);
+	close(fp);
 
 	return (1);
 }
