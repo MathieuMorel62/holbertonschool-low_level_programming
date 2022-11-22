406a51fd4d7fe142744723b61e6cf905ff28f228
MathieuMorel62 <mormth@aol.com>
2022-11-22 00:58:58 -0800

    add loop, close, write functions

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -10,29 +10,43 @@
 
 int main(int argc, char *argv[])
 {
-	int file_from, file_to;
-/* number of argument */
+	int file_from, file_to, readed;
+	char buffer[1024];
+
+/* --------------------------- NUMBER OF ARGUMENT --------------------------*/
 	if (argc != 3)
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-	exit(97);
-/* read */
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+/* ---------------------------------- READ ---------------------------------*/
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-	exit(98);
-/* create */
+		exit(98);
+	}
+/* --------------------------------- CREATE --------------------------------*/
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (file_to == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-	exit(99);
-/* close */
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+/* ---------------------------------- WRITE --------------------------------*/
+	while ((readed = read(file_from, buffer, 1024)) > 0)
+	{
+		if (write(file_to, buffer, readed) != readed || file_to == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+	if (readed == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+/* ----------------------------------- CLOSE --------------------------------*/
 	if (close(file_from) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-	exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
 
 	if (close(file_to) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
-	exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
 
 	return (0);
 }
