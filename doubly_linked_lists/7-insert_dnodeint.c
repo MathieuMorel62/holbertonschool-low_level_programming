342dae25fb5588d141bbfc3e20693f17f510d75d
MathieuMorel62 <mormth@aol.com>
2022-11-22 06:43:19 -0800

    add conditon if on loop

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -36,11 +36,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	else
 	{
 		for (i = 0; i < (idx - 1) && tmp; i++)
+		{
 			tmp = tmp->next;
 
-		if (tmp == NULL)
-			return (NULL);
-
+			if (tmp == NULL)
+				return (NULL);
+		}
 		new->prev = tmp;
 		new->next = tmp->next;
 		tmp->next->prev = new;
