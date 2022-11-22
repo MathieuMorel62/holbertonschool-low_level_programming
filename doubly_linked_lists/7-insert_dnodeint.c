22517b775c7966bbfb8f1b6990643457e766cc97
MathieuMorel62 <mormth@aol.com>
2022-11-22 06:45:45 -0800

    add idx decrementation after the loop

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -42,6 +42,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			if (tmp == NULL)
 				return (NULL);
 		}
+		idx--;
+
 		new->prev = tmp;
 		new->next = tmp->next;
 		tmp->next->prev = new;
