93abfe084d099e047a77e28c9209bb895d6ef360
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-01-02 13:25:27 +0100

    betty coding style

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -3,9 +3,11 @@
 /**
  * linear_search - function that searches for a value in an array
  * of integers using the Linear search algorithm.
+ *
  * @array: Pointer to the first element of the array to search in.
  * @size: Number of elements in the array.
  * @value: Value to search for.
+ *
  * Return: The index of the first occurrence of value in the array,
  * or -1 if value is not present in the array or if array is NULL.
  */
