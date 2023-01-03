c985e1c2cd35e6eca3da67db5fc0546c5fe57349
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-01-03 12:56:17 +0100

    classification of files in order

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints the alphabet in lowercase
+ *              except q and e
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		if (x != 'e' && x != 'q')
+			putchar(x);
+	}		
+	putchar('\n');
+
+	return (0);
+}
+
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints all single digit numbers
+ *              of base 10 starting from 0
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		
+		printf("%d", num);
+
+	putchar('\n');
+
+	return (0);
+}
+
