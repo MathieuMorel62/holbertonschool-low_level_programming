f730cf26562c0f29a99ae7721e6b26bf5cc6eb05
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-01-03 18:37:07 +0100

    Implement the print_number function to print an integer

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,3 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* ----------- PROTOTYPES ---------- */
+
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -10,4 +15,6 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
+void print_number(int n);
 
+#endif
