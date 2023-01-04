deb8e0b7b93859282360634c239df292254faad0
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-01-04 17:36:46 +0100

    Uses a ternary expression to manage the sign of the number

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -4,26 +4,46 @@
  * _atoi - Convert a string to a integer.
  * @s: The string to convert.
  *
- * Return: The converted integer.
+ * Return: The integer representation of the number in the string.
 */
 
 int _atoi(char *s)
 {
-	int index = 0, result = 0, sign = 1;
+	int index = 0, found_number = 0, result = 0, sign = 1, neg = 0;
 
-	/* Skip leading whitespace and sign characters */
-	while (s[index] == ' ' || s[index] == '+' || s[index] == '-')
+	while (s[index] != '\0')
 	{
-		if (s[index] == '-')
+		if (s[index] == ' ')
+			;
+
+		else if (s[index] == '+' || s[index] == '-')
 		{
-			sign = -1;
+			if (!found_number)
+				sign = (s[index] == '+') ? 1 : -1;
+
+			while (s[index] == ' ')
+				index++;
+
+			if (sign == -1)
+				neg++;
+		}
+		else if (s[index] >= '0' && s[index] <= '9')
+		{
+			found_number = 1;
+			result = result * 10 + (s[index] - '0');
+		}
+		else
+		{
+			if (found_number)
+				break;
 		}
 		index++;
 	}
-	while (s[index] >= '0' && s[index] <= '9')
-	{
-		result = result * 10 + (s[index] - '0');
-		index++;
-	}
-	return (result * sign);
+	if (!found_number)
+		return (0);
+
+	if (neg % 2 != 0)
+		result = -result;
+
+	return (result);
 }
