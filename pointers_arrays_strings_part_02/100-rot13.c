18ccedbd3475824b9adbd74a57c0f6c821b18543
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-01-05 21:04:29 +0100

    add function rot13

---
+++ pointers_arrays_strings_part_02/100-rot13.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * rot13 - encode a string using the ROT13
+ * @s: the string to encode
+ *
+ * Return: a pointer to the encoded string
+ */
+
+char *rot13(char *s)
+{
+	int i, j;
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; alphabet[j] != '\0'; j++)
+		{
+			if (s[i] == alphabet[j])
+			{
+				s[i] = rot13[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
