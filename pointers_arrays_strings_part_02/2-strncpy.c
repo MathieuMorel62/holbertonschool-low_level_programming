5785825eea5e4c0bc74a83c8b5c62729e5652ecf
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-01-08 15:09:23 +0100

    classification of files on GitHub

--- pointers_arrays_strings_part_02/2-strncpy.c
+++ pointers_arrays_strings_part_02/2-strncpy.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-#include <stddef.h>
 
 /**
  * _strncpy - Copies a string
--- pointers_arrays_strings_part_02/main.h
+++ pointers_arrays_strings_part_02/main.h
@@ -1,6 +1,14 @@
 #ifndef MAIN_H
-#define MAIN_H
+#define MAIN.H
 
+/* -------------- LIBRARY --------------*/
+#include <limits.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stddef.h>
+
+/* -------------- PROTOTYPES ------------- */
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -10,5 +18,6 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *s);
+void print_number(int n);
 
-#ifndef
+#endif
