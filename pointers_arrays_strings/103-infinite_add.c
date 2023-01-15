4ab34a95c4fdfeb777d8b63fdf5e76dc6d72eb9e
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-01-15 21:31:31 +0100

    function that adds two numbers.

--- pointers_arrays_strings/103-infinite_add.c
+++ pointers_arrays_strings/103-infinite_add.c
@@ -7,41 +7,47 @@
  * @r: the buffer that the function will use to store the result
  * @size_r: the buffer size
  *
- * Returns: a pointer to the result
+ * Return: pointer to the result buffer or 0
 */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1 = strlen(n1);
-	int len2 = strlen(n2);
-	int maxlen = (len1 > len2) ? len1 : len2;
-	int carry = 0;
-	int sum, digit1, digit2;
-	int index;
+	int len1, len2, index, swap_index, carry, sum;
 
-	if (size_r < maxlen + 2)
+	for (len1 = 0; n1[len1]; len1++)
+		;
+	for (len2 = 0; n2[len2]; len2++)
+		;
+	if (len1 > size_r || len2 > size_r)
 		return (0);
 
-	if (carry > 0)
-	{
-		maxlen++;
-		r[0] = carry + '0';
-	}
-	r[maxlen] = '\0';
+	carry = 0;
 
-	for (index = 1; index <= maxlen; index++)
+	for (len1 -= 1, len2 -= 1, index = 0; index < size_r - 1;
+		len1--, len2--, index++)
 	{
-		digit1 = (index <= len1) ? n1[len1 - index] - '0' : 0;
-		digit2 = (index <= len2) ? n2[len2 - index] - '0' : 0;
-		sum = digit1 + digit2 + carry;
+		sum = carry;
+
+		if (len1 >= 0)
+			sum = sum + n1[len1] - '0';
+		if (len2 >= 0)
+			sum = sum + n2[len2] - '0';
+		if (len1 < 0 && len2 < 0 && sum == 0)
+			break;
+
 		carry = sum / 10;
-		r[maxlen - index] = (sum % 10) + '0';
+		r[index] = sum % 10 + '0';
 	}
-	if (carry > 0)
+	r[index] = '\0';
+
+	if (len1 >= 0 || len2 >= 0 || carry)
+		return (0);
+
+	for (index -= 1, swap_index = 0; swap_index < index; index--, swap_index++)
 	{
-		r[0] = carry + '0';
-		return (r);
+		carry = r[index];
+		r[index] = r[swap_index];
+		r[swap_index] = carry;
 	}
-	else
-		return (r + 1);
+	return (r);
 }
