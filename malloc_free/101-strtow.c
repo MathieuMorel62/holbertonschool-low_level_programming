75e6bb19c4a82918e705e12ec53ee157b9cb9c93
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-01-23 02:01:09 +0100

    add count words function for count the number of words in a string

--- malloc_free/101-strtow.c
+++ malloc_free/101-strtow.c
@@ -1,6 +1,32 @@
 #include "main.h"
 
 /**
+ * count_words - count the number of words in a string
+ * @s: string to evaluate
+ *
+ * Return: number of words
+ */
+int count_words(char *s)
+{
+	int flag, i, word_count;
+
+	flag = 0;
+	word_count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == ' ')
+			flag = 0;
+		else if (flag == 0)
+		{
+			flag = 1;
+			word_count++;
+		}
+	}
+	return (word_count);
+}
+
+/**
  * strtow - splits a string into words
  * @str: the string to split
  *
@@ -9,44 +35,42 @@
 
 char **strtow(char *str)
 {
-	int i, j, k, word_count = 0, start, end;
-	char **words;
+	char **words, *temp;
+	int i, word_index = 0, len = 0, word_count, start, end, word_length;
 
-	if (str == NULL || strlen(str) == 0)
+	while (*(str + len))
+		len++;
+
+	word_count = count_words(str);
+	if (word_count == 0)
 		return (NULL);
 
-	for (i = 0; str[i]; i++)
-		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
-			word_count++;
-
-	words = malloc(sizeof(char *) * (word_count + 1));
-	if (!words)
+	words = (char **) malloc(sizeof(char *) * (word_count + 1));
+	if (words == NULL)
 		return (NULL);
 
-	j = 0;
-	for (i = 0; str[i]; i++)
+	for (i = 0; i <= len; i++)
 	{
-		if (str[i] != ' ')
+		if (str[i] == ' ' || str[i] == '\0')
 		{
-			start = i;
-			while (str[i] && str[i] != ' ')
-				i++;
-			end = i;
-			if (start == end)
-				continue;
-			words[j] = malloc(sizeof(char) * (end - start + 1));
-			if (!words[j])
+			if (word_length)
 			{
-				for (k = 0; k < j; k++)
-					free(words[k]);
-				free(words);
-				return (NULL);
+				end = i;
+				temp = (char *) malloc(sizeof(char) * (word_length + 1));
+				if (temp == NULL)
+					return (NULL);
+
+				while (start < end)
+					*temp++ = str[start++];
+				*temp = '\0';
+				words[word_index] = temp - word_length;
+				word_index++;
+				word_length = 0;
 			}
-			strncpy(words[j], str + start, end - start);
-			words[j][end - start] = '\0';
-			j++;
 		}
+		else if (word_length++ == 0)
+			start = i;
 	}
-	words[j] = NULL;
+	words[word_index] = NULL;
 	return (words);
 }
