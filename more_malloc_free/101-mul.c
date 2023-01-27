57ae4ec39bff5160932ed852cf8d7557a4883048
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-01-27 15:04:08 +0100

    test function

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -1,71 +1,31 @@
 #include "main.h"
 
-/**
-  * _isdigit - tells if the string consists of digits
-  * @argv: pointer to current item in argument
-  * Return: return 0 if all digits, 1 if not all digits.
-  */
-int _isdigit(char *argv)
-{
-	int i;
-
-	i = 0;
-	while (argv[i])
-	{
-		if (argv[i] >= '0' && argv[i] <= '9')
-			i++;
-		else
-			return (1);
-	}
-	return (0);
-}
-/**
-  * _atoi - converts a string of ascii digits to the values they represent
-  * @s: pointer to the source string
-  * Return: value of digits
-  */
-int _atoi(char *s)
-{
-	int i, result;
-
-	i = result = 0;
-	while (s[i])
-	{
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			result *= 10;
-			result += (s[i] - '0');
-		}
-		i++;
-	}
-	return (result);
-}
-/**
-  * main - main function call
-  * @argc: argument count
-  * @argv: 2D array of arguments
-  * Return: return 0 on success, 98 on failure
-  */
 int main(int argc, char *argv[])
 {
-	int i, num1, num2, result;
+    int num1, num2, result, len1, len2;
+    char *resultStr;
 
-	if (argc != 3)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	for (i = 1; i < argc; i++)
-	{
-		if (_isdigit(argv[i]))
-		{
-			printf("Error\n");
-			exit(98);
-		}
-	}
-	num1 = _atoi(argv[1]);
-    num2 = _atoi(argv[2]);
+    if (argc != 3)
+    {
+        printf("Error\n");
+        exit(98);
+    }
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[2]);
     result = num1 * num2;
-    printf("%d\n", result);
-	return (0);
+
+    len1 = snprintf(NULL, 0, "%d", num1);
+    len2 = snprintf(NULL, 0, "%d", num2);
+    resultStr = malloc((len1 + len2 + 1) * sizeof(char));
+    if (resultStr == NULL)
+    {
+        printf("Error\n");
+        exit(98);
+    }
+
+    sprintf(resultStr, "%d", result);
+    printf("%s\n", resultStr);
+
+    free(resultStr);
+    return (0);
 }
