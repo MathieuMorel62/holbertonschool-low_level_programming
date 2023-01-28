21b42cbffae2587083e8aa879d6b7e72ac8a13ae
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-01-28 17:48:35 +0100

    update readme file

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,8 +1,11 @@
 #ifndef FUNCTION_POINTERS_H_
 #define FUNCTION_POINTERS_H_
+
+/************ LIBRARY ***********/
 #include <stdio.h>
 #include <stdlib.h>
 
+/********** PROTOTYPES **********/
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
