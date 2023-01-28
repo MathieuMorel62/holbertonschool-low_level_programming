78153e653a18cbc39e718393cc5bd4be49c2c610
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-01-28 18:28:43 +0100

    64-bit program in assembly that prints Hello, World

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,18 +1,17 @@
-section .data
-    hello db 'Hello, World',10
+section     .data
+	msg db 'Hello, World',0xa
+	msglen equ $ - msg
 
 section .text
-    global _start
+	global _start
 
 _start:
-    ; write(1, hello, 12)
-    mov edx, 12    ; message length
-    mov ecx, hello ; message to write
-    mov ebx, 1     ; file descriptor (stdout)
-    mov eax, 4     ; system call for sys_write
-    int 0x80       ; call kernel
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, msg
+	mov rdx, msglen
+	syscall
 
-    ; exit(0)
-    mov eax, 1     ; system call for sys_exit
-    xor ebx, ebx   ; exit status (0)
-    int 0x80       ; call kernel
+	mov rax, 60
+	xor rdi, rdi
+	syscall
