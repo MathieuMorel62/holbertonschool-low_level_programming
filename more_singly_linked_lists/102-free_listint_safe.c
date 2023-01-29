71669e37af3abb970132f5724a34924555ee3df4
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-01-29 02:08:15 +0100

    fix bug for function that frees a listint_t list.

--- more_singly_linked_lists/102-free_listint_safe.c
+++ more_singly_linked_lists/102-free_listint_safe.c
@@ -1,28 +1,68 @@
 #include "lists.h"
 
 /**
- * free_listint_safe - function that frees a listint_t list.
- * @h:
- *
- * Return:
+ * free_looped_list - function that frees a looped list of multiple elements.
+ * @head: pointer to the head of the list
 */
 
-size_t free_listint_safe(listint_t **h) 
+void free_looped_list(listp_t **head)
 {
-	listint_t *temp = *h, *current = *h;
-	size_t len = 0;
+	listp_t *temp;
+	listp_t *current;
 
-	while (current)
+	if (head != NULL)
 	{
-		len++;
-		if (current->next == *h)
+		current = *head;
+		while ((temp = current) != NULL)
 		{
-			current->next = NULL;
+			current = current->next;
+			free(temp);
 		}
-		temp = current;
-		current = current->next;
-		free(temp);
+		*head = NULL;
+	}
+}
+
+/**
+ * free_listint_safe - function that frees a listint_t list.
+ * @h: pointer to the head of the list
+ *
+ * Return:  the number of nodes in the list
+*/
+
+size_t free_listint_safe(listint_t **h)
+{
+	size_t nnodes = 0;
+	listp_t *pointer_list, *new, *add;
+	listint_t *current;
+
+	pointer_list = NULL;
+	while (*h != NULL)
+	{
+		new = malloc(sizeof(listp_t));
+		if (new == NULL)
+			exit(98);
+
+		new->p = (void *)*h;
+		new->next = pointer_list;
+		pointer_list = new;
+
+		add = pointer_list;
+		while (add->next != NULL)
+		{
+			add = add->next;
+			if (*h == add->p)
+			{
+				*h = NULL;
+				free_looped_list(&pointer_list);
+				return (nnodes);
+			}
+		}
+		current = *h;
+		*h = (*h)->next;
+		free(current);
+		nnodes++;
 	}
 	*h = NULL;
-	return (len);
+	free_looped_list(&pointer_list);
+	return (nnodes);
 }
