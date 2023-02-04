475bdb2d56c94c9d9dba0b160aa00587f1ec23c4
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-02-05 00:54:13 +0100

    add keygen function

--- doubly_linked_lists/103-keygen.c
+++ doubly_linked_lists/103-keygen.c
@@ -3,51 +3,104 @@
 #include <time.h>
 
 /**
- * generate_key_char - Function that generates a character key from an entry.
- * @input: Pointer to the entrance
- * @size: Size of the entrance
- *
- * Return: Key generated
-*/
-int generate_key_char(char *input, int size)
+ * function1 - function1 - Calculates the second character of the key
+ * @input_length: The size of the user input
+ * @input_array: The array of arguments passed in from the command line
+ * Return: The position of the character in the "posvalues" array
+ */
+int function1(int input_length, char *input_array[])
 {
-	int car = 0, count = 0;
+	int index = 0, total_value = 0, result;
 
-	for ( ; count < size; count++)
-		car = car + input[count];
-
-	srand(input[0] ^ 14);
-	return ((car ^ (rand() & (79 ^ 85 ^ 239 ^ 229 ^ 14))) & 63);
+	for (; index < input_length; index++)
+		total_value += input_array[1][index];
+	result = (total_value ^ 79) & 63;
+	return (result);
 }
 
 /**
- * main - Main function
- * @argc: Number of arguments
- * @argv: Table of arguments
- *
- * Return: Return code
-*/
+ * function2 - Calculates the third character of the key
+ * @input_length: The size of the user input
+ * @input_array: The array of arguments passed in from the command line
+ * Return: The position of the character in the "posvalues" array
+ */
+int function2(int input_length, char *input_array[])
+{
+	int index = 0, product = 1, result;
+
+	for (; index < input_length; index++)
+		product = input_array[1][index] * product;
+	result = (product ^ 85) & 63;
+	return (result);
+}
+
+/**
+ * function3 - Calculates the fourth character of the key
+ * @input_str: The array of arguments passed in from the command line
+ * Return: The position of the character in the "posvalues" array
+ */
+int function3(char *input_str[])
+{
+	int result, max_char = 0, index = 0;
+
+	for (; input_str[1][index]; index++)
+	{
+		if (max_char < input_str[1][index])
+			max_char = input_str[1][index];
+	}
+	srand(max_char ^ 14);
+	result = rand() & 63;
+	return (result);
+}
+
+/**
+ * function4 - Calculates the fifth character of the key
+ * @input_str: The array of arguments passed in from the command line
+ * Return: The position of the character in the "posvalues" array
+ */
+int function4(char *input_str[])
+{
+	int result, sum = 0, input = 0;
+
+	for (; input_str[1][input]; input++)
+		sum = sum + (input_str[1][input] * input_str[1][input]);
+	result = (sum ^ 239) & 63;
+
+	return (result);
+}
+
+/**
+ * main - Generates a key for crackme 5 according to the input arguments
+ * @argc: Number of arguments in the entry
+ * @argv: Table that stores the arguments
+ * Return: 0 if success
+ */
 int main(int argc, char *argv[])
 {
-	char values[] = "A-CHRDw87lNS0E9B2TibgpnMVys5Xzvt0GJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+	char posvalues[] =
+		"A-CHRDw87lNS0E9B2TibgpnMVys5Xzvt0GJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 	char key[7];
-	int i = 0;
-	int size = 0;
+	int index = 0, sum = 0, value;
 
 	if (argc > 1)
 	{
-		while (argv[1][size])
-			size++;
+		while (argv[1][index])
+			index++;
 
-		key[0] = values[size ^ (59 & 63)];
-		key[1] = values[generate_key_char(argv[1], size)];
-		key[2] = values[generate_key_char(argv[1], size)];
-		key[3] = values[generate_key_char(argv[1], size)];
-		key[4] = values[generate_key_char(argv[1], size)];
-		key[5] = values[generate_key_char(argv[1], size)];
+		key[0] = posvalues[(index ^ 59) & 63];
+		key[1] = posvalues[function1(index, argv)];
+		key[2] = posvalues[function2(index, argv)];
+		key[3] = posvalues[function3(argv)];
+		key[4] = posvalues[function4(argv)];
 
-		for (i = 0; i < 6; i++)
-			printf("%c", key[i]);
+		for (; sum < argv[1][0]; sum++)
+			value = rand();
+
+		value = (value ^ 229) & 63;
+		key[5] = posvalues[value];
+
+		for (index = 0; index < 6; index++)
+			printf("%c", key[index]);
 	}
 	return (0);
 }
